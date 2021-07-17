84e5f9fea9c8d77a73e265661e7786d2a1fc5b92
Nolan Caine <nolancaine@gmail.com>
2021-07-16 21:20:24 -0600

    Added selectors and functionality for saved cover view

--- src/main.js
+++ src/main.js
@@ -9,10 +9,16 @@ var makeNewButton = document.querySelector('.make-new-button');
 var homeButton = document.querySelector('.home-button');
 var randomCoverButton = document.querySelector('.random-cover-button');
 var saveCoverButton = document.querySelector('.save-cover-button');
+var viewSavedButton = document.querySelector('.view-saved-button');
 
 var formView = document.querySelector('.form-view');
 var homeView = document.querySelector('.home-view');
+var savedView = document.querySelector('.saved-view');
 
+var mainCover = document.querySelector('.main-cover');
+var miniCover = document.querySelector('.mini-cover');
+
+var savedCoversSection = document.querySelector(".saved-covers-section");
 
 // We've provided a few variables below
 var savedCovers = [
@@ -25,6 +31,7 @@ window.addEventListener('load', loadRandomCover);
 randomCoverButton.addEventListener('click', loadRandomCover);
 randomCoverButton.addEventListener('click', createCover);
 makeNewButton.addEventListener('click', changeToFormView);
+viewSavedButton.addEventListener('click', changeToSavedPostersView);
 
 // Create your event handlers and other functions here 👇
 
@@ -60,12 +67,43 @@ function loadRandomCover() {
   loadRandomTagline2();
 };
 function changeToFormView() {
+  homeView.classList.add("hidden");
+  savedView.classList.add("hidden");
+  randomCoverButton.classList.add("hidden");
+  saveCoverButton.classList.add("hidden");
   formView.classList.remove("hidden");
+  homeButton.classList.remove("hidden");
+};
+function changeToSavedPostersView() {
+  formView.classList.add("hidden");
   homeView.classList.add("hidden");
   randomCoverButton.classList.add("hidden");
   saveCoverButton.classList.add("hidden");
+  savedView.classList.remove("hidden");
   homeButton.classList.remove("hidden");
+  displaySavedCovers();
 };
+
+function displaySavedCovers() {
+  // mainCoversavedCovers[i].tagline2.classList.remove("main-cover");
+  savedCoversSection.innerHTML = `
+  <section class="mini-cover">
+  <img src=${savedCovers[0].cover}>
+  <h2 class="cover-title">${savedCovers[0].title}</h2>
+  <h3 class="tagline">A tale of ${savedCovers[0].tagline1} and ${savedCovers[0].tagline2}</h3>
+  </section>
+  `;
+  // miniCover.style.backgroundImage = `"url('${savedCovers[0].cover}')"`;
+};
+
+
+
+
+
+// savedCovers[i].cover
+// savedCovers[i].title
+// savedCovers[i].tagline1
+// savedCovers[i].tagline2
 
 // Is there a way to make hover behavior static using only JS?
 // If so, could be applied to last clicked button - apply to iteration 2
